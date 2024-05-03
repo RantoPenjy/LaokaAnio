a38010ecc98539608066e4382aa53f883ce7b3b3
Rakotondramanana Ranto <rakotondramanana.rantoniaina@gmail.com>
2024-05-03 14:07:53 +0300

    remove relation OneToMany plats

--- src/Entity/DayType.php
+++ src/Entity/DayType.php
@@ -18,17 +18,6 @@ class DayType
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    /**
-     * @var Collection<int, Plat>
-     */
-    #[ORM\OneToMany(targetEntity: Plat::class, mappedBy: 'day_type')]
-    private Collection $plats;
-
-    public function __construct()
-    {
-        $this->plats = new ArrayCollection();
-    }
-
     public function getId(): ?int
     {
         return $this->id;
@@ -46,33 +35,4 @@ class DayType
         return $this;
     }
 
-    /**
-     * @return Collection<int, Plat>
-     */
-    public function getPlats(): Collection
-    {
-        return $this->plats;
-    }
-
-    public function addPlat(Plat $plat): static
-    {
-        if (!$this->plats->contains($plat)) {
-            $this->plats->add($plat);
-            $plat->setDayType($this);
-        }
-
-        return $this;
-    }
-
-    public function removePlat(Plat $plat): static
-    {
-        if ($this->plats->removeElement($plat)) {
-            // set the owning side to null (unless already changed)
-            if ($plat->getDayType() === $this) {
-                $plat->setDayType(null);
-            }
-        }
-
-        return $this;
-    }
 }
