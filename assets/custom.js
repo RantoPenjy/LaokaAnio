322407785d7583b79c3ba24a4abc2f5818ae4a1f
Rakotondramanana Ranto <rakotondramanana.rantoniaina@gmail.com>
2024-05-03 22:24:53 +0300

    Change design for responsiveness

--- assets/custom.js
+++ assets/custom.js
@@ -5,25 +5,34 @@ jQuery(function(){
         e.preventDefault();
         $("#randomBtnSpinner").removeAttr('hidden').show();
         $("#randomBtnText").hide();
+        $("#showRecipeLink").hide();
         $.ajax({
             url: window.location.origin + "/plat/random",
             type: "GET",
             success: function (response) {
-                $("#randomBtnText").show().text('Hisafidy hafa');
-                $("#randomBtnSpinner").hide();
-                let result = "👉 " + response + " 👈";
-                $("#randomResult").addClass('animate__animated animate__zoomIn').text(result);
-                $("#randomResult").css("opacity", 1);
-                /*anime({
-                    targets: ["#randomResult", "#showRecipeLink"],
-                    opacity: [0, 1],
-                    duration: 1000,
-                    easing: 'easeInOutQuad',
-                })*/
+                setTimeout(function() {
+                    $("#randomBtnText").show().text('Hisafidy hafa');
+                    $("#randomBtnSpinner").hide();
+                    let result;
+                    if (window.innerWidth > 580) {
+                        result = "👉 " + response + " 👈";
+                    }
+                    else {
+                        result = response;
+                    }
+                    $("#randomResult").addClass('animate__animated animate__zoomIn').text(result);
+                    $("#randomResult").css("opacity", 1);
+                    $("#showRecipeLink").css("opacity", 1).css("cursor", "pointer").show();
+
+                }, 2000);
             },
             error: function(xhr, textStatus, errorThrown) {
                 console.log('Ajax request failed.');
             }
         })
     })
+
+    if ($(window).width() > 580) {
+        console.log($("#randomResult").text())
+    }
 })
